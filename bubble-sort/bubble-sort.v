e80607975cda960d1fbc5121869817aa2301cf31
Your Name <nonaka@mac.com>
2012-04-14 17:33:55 +0900

    added more code.

--- bubble-sort/bubble-sort.v
+++ bubble-sort/bubble-sort.v
@@ -11,17 +11,18 @@ Fixpoint min_nat (l : list nat) : nat :=
   match l with
     | nil => 0
     | x::nil => x
-    | x::xs => 
-      if (x <= (min_nat xs)) 
-        then x
-        else min_nat xs
+    | x::xs  => match (leb x (min_nat xs)) with
+                  | true => x
+                  | false => min_nat xs
+                end
   end.
 
 
 Fixpoint bubble_sort_nat (l : list nat) : list nat :=
   match l with
     | nil => nil
-    | _  => undfied
+    | _  => (min_nat l)::(bubble_sort_nat
+      (remove_one_nat (min_nat l) l))
   end.
 
     
--- bubble-sort/test-remove.v
+++ bubble-sort/test-remove.v
@@ -1,3 +1,4 @@
 Require Import List.
-
-Eval compute in remove 3 3::2::1::nil.
+Require Import Arith.
+Open Scope list_scope.
+Eval compute in remove eq_nat_dec 3 (3::2::1::nil).
